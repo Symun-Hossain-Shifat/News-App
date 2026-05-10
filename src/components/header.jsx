f89e22f46379a48730113822d7b99cccf9476c3c
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-05-10 15:46:37 +0600

    url updated final

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ function headerpage () {
     <div className='items-center justify-center text-center py-10 space-y-2'> 
       
       <Image src={logo} alt="logo Image " className='mx-auto'  />
-      <h1 className='text-gray-700 text-2xl font-semibold'>Journalism Without Fear or Favour</h1>
+      <h1 className='text-gray-700 text-2xl font-semibold'>Journalism Without Fear and Favour</h1>
       <p>{format(new Date(2014, 1, 11), "EEEE , MMM dd , yyyy" )} </p>
 
       
